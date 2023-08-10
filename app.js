7430b360c82a68001ad4decf21de3f1d56f34966
Jeff FORESTAL <forestaljeff00@gmail.com>
2023-08-10 03:28:44 -0400

    add route custom route=> '/:customLisName' and partial refactor

--- app.js
+++ app.js
@@ -73,12 +73,54 @@ app.post("/delete", async (req, res)=>{
     res.redirect("/");
 });
 
-
 // USING ROUTE PARAMETERS
-app.get("/:name",  (req, res)=>{
-    let result = undefined;
-    res.render("index.ejs", {tasks: result});
+// CUSTOM LIST
+
+const listSchema = new mongoose.Schema({
+    listName: String,
+    tasks: Array
 });
+
+const List = mongoose.model("List", listSchema);
+
+// LAST WORK ADDING CUSTOM LIST DOCUMENTS
+app.get("/:customListName", async (req, res)=>{
+    
+    // CHECK IF LIST EXIST IN DATABASE
+    const listName = req.params.customListName;
+
+    // console.log(listName);
+    try {
+        const existingList = await List.findOne({listName: listName});
+
+        if(existingList === null){
+            const newList = new List({
+                listName: listName,
+                tasks: []
+            });
+            newList.save();
+
+            res.redirect("/"+ listName);
+        }else{
+
+            console.log(existingList);
+            // res.render("index.ejs", {
+            //     listName: existingList.listName,
+            //     tasks: existingList.tasks
+            // });
+        }
+
+    } catch (error) {
+        console.log("Error: " + error.message);
+        res.status(500);
+    }
+});
+
+
+
+
+
+
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
